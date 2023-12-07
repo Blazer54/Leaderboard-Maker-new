a38fbd51ac50f450f3a5616b5763e4620f27dadd
Blazer54 <pmf.marques.gva@gmail.com>
2023-12-07 08:46:57 +0100

    ajout de la classe equipe

---
+++ ProjetPerso/Equipe.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetPerso
+{
+
+    internal class Equipe
+    {
+        string nomJoueur;
+
+        public Equipe(string nomJoueur)
+        {
+            this.nomJoueur = nomJoueur;
+        }
+
+        public override string ToString()
+        {
+            return $"{nomJoueur}";
+        }
+    }
+}
--- ProjetPerso/FrmModifier.Designer.cs
+++ ProjetPerso/FrmModifier.Designer.cs
@@ -28,10 +28,19 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // FrmModifier
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form2";
+            this.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.ClientSize = new System.Drawing.Size(1077, 564);
+            this.Name = "FrmModifier";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Modifier";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
