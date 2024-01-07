6d382227b1d7dbbb6771aaf02b41bd297cda2bbb
Blazer54 <pmf.marques.gva@gmail.com>
2024-01-07 19:28:00 +0100

    fix bug form Ajouter une partie

--- ProjetPerso/FrmAjouterunepartie.cs
+++ ProjetPerso/FrmAjouterunepartie.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using TextBox = System.Windows.Forms.TextBox;
 
 namespace ProjetPerso
 {
@@ -15,6 +16,9 @@ namespace ProjetPerso
         List<Equipe> equipes = new List<Equipe>();
         List<Joueur> joueurs = new List<Joueur>();
         List<Systpoint> systpoint = new List<Systpoint>();
+        List<Partie> parties = new List<Partie>();
+        List<TextBox> listTextboxPlacement = new List<TextBox>();
+        List<TextBox> listTextboxKill = new List<TextBox>();
         public FormAjouterPartie(List<Equipe> equipe, List<Joueur> joueur, List<Systpoint> systpoints)
         {
             InitializeComponent();
@@ -35,11 +39,11 @@ namespace ProjetPerso
             for (int i = 0; i <= equipes.Count / 2 - 1; i++)
             {
                 //on définit une variable qui va être la position de x des éléments
-                int posX = 180;
+                int posX = 90;
                 // Créer un Label
                 Label label = new Label();
                 //on dit que le texte du label sera le nom du joueur à l'index qui est i
-                label.Text = $"{equipes[i].NomEquipe}";
+                label.Text = $"placement {equipes[i].NomEquipe}";
                 //on donne un nom pour le label
                 label.Name = "lblJoueur" + i.ToString();
                 //on donne la position de où est ce que le label apparaitra
@@ -47,12 +51,30 @@ namespace ProjetPerso
                 this.Controls.Add(label);
 
                 //on ajoute une nouvelle textbox
-                TextBox textBoxJoueur = new TextBox();
+                TextBox textBoxPlacement = new TextBox();
                 //on définit la position
-                textBoxJoueur.Location = new System.Drawing.Point(posX + 125, posY);
-                textBoxJoueur.Size = new System.Drawing.Size(100, 20);
-                textBoxJoueur.Name = "tbxJoueur" + i.ToString(); // Nommer la TextBox de manière unique
-                this.Controls.Add(textBoxJoueur);
+                textBoxPlacement.Location = new System.Drawing.Point(posX + 125, posY);
+                textBoxPlacement.Size = new System.Drawing.Size(100, 20);
+                textBoxPlacement.Name = "tbxJoueur" + i.ToString(); // Nommer la TextBox de manière unique
+                this.Controls.Add(textBoxPlacement);
+
+                listTextboxPlacement.Add(textBoxPlacement);
+
+                Label labelKill = new Label();
+                labelKill.Text = $"Kill globaux";
+                labelKill.Location= new System.Drawing.Point(posX + 275, posY + 30);
+                this.Controls.Add(labelKill);
+
+                //on ajoute une nouvelle textbox
+                TextBox textBoxKill = new TextBox();
+                //on définit la position
+                textBoxKill.Location = new System.Drawing.Point(posX + 250, posY);
+                textBoxKill.Size = new System.Drawing.Size(100, 20);
+                textBoxKill.Name = "tbxJoueur" + i.ToString(); // Nommer la TextBox de manière unique
+                this.Controls.Add(textBoxKill);
+
+                listTextboxKill.Add(textBoxKill);
+
             }
 
 
@@ -60,23 +82,40 @@ namespace ProjetPerso
             for (int i = equipes.Count / 2; i <= equipes.Count - 1; i++)
             {
                 //déclaration de la variable pour la cordonée X
-                int posX = 250;
+                int posX = 350;
                 // Créer un Label
                 Label label = new Label();
                 //on définit ses propriétées
-                label.Text = $"{equipes[i].NomEquipe}";
+                label.Text = $"placement {equipes[i].NomEquipe}";
                 label.Name = "lblJoueur" + i.ToString();
-                label.Location = new System.Drawing.Point(posX + 275, posY);
+                label.Location = new System.Drawing.Point(posX + 135, posY);
                 //on l'ajoute à la liste des labels
                 this.Controls.Add(label);
 
                 //on créer une nouvelle textebox
-                TextBox textBoxJoueur = new TextBox();
+                TextBox textBoxPlacement = new TextBox();
                 //on indique la position de l'élément
-                textBoxJoueur.Location = new System.Drawing.Point(posX + 350, posY);
-                textBoxJoueur.Size = new System.Drawing.Size(100, 20);
-                textBoxJoueur.Name = "tbxJoueur" + i.ToString(); // Nommer la TextBox de manière unique
-                this.Controls.Add(textBoxJoueur);
+                textBoxPlacement.Location = new System.Drawing.Point(posX + 250, posY);
+                textBoxPlacement.Size = new System.Drawing.Size(100, 20);
+                textBoxPlacement.Name = "tbxJoueur" + i.ToString(); // Nommer la TextBox de manière unique
+                this.Controls.Add(textBoxPlacement);
+
+                listTextboxPlacement.Add(textBoxPlacement);
+
+                Label labelKill = new Label();
+                labelKill.Text = $"Kill globaux";
+                labelKill.Location = new System.Drawing.Point(posX + 450, posY + 30);
+                this.Controls.Add(labelKill);
+
+                //on ajoute une nouvelle textbox
+                TextBox textBoxKill = new TextBox();
+                //on définit la position
+                textBoxKill.Location = new System.Drawing.Point(posX + 425, posY);
+                textBoxKill.Size = new System.Drawing.Size(100, 20);
+                textBoxKill.Name = "tbxJoueur" + i.ToString(); // Nommer la TextBox de manière unique
+                this.Controls.Add(textBoxKill);
+
+                listTextboxKill.Add(textBoxKill);
 
                 posY = posY + 50;
             }
@@ -85,8 +124,21 @@ namespace ProjetPerso
         private void btnValider_Click(object sender, EventArgs e)
         {
             this.Hide();
-            FrmMenuPrincipale frmMenuPrincipale = new FrmMenuPrincipale(systpoint, equipes, joueurs);
-            frmMenuPrincipale.ShowDialog();
+            AjouterPartie(listTextboxPlacement, listTextboxKill,parties, equipes)
+            FrmMenuPrincipale2 frmMenuPrincipale2 = new FrmMenuPrincipale2(systpoint, equipes, joueurs, Partie);
+            frmMenuPrincipale2.ShowDialog();
         }
+
+        static void AjouterPartie(List<TextBox> listTextboxPlacement, List<TextBox> listTextboxKill, List<Partie> parties, List<Equipe> equipes)
+        {
+            for (int i = 0; i < listTextboxPlacement.Count; i++) 
+            {
+                int placement = Convert.ToInt32(listTextboxPlacement[i].Text);
+                int kill = Convert.ToInt32(listTextboxKill[i].Text);
+                parties.Add(new Partie(equipes[i].NomEquipe,placement, kill));
+            }
+        }
+
+
     }
 }
--- ProjetPerso/Partie.cs
+++ ProjetPerso/Partie.cs
@@ -9,6 +9,23 @@ namespace ProjetPerso
     internal class Partie
     {
         private string nomEquipe;
+        private int placement;
+        private int kill;
 
+        public Partie(string nomEquipe, int placement, int kill) 
+        {
+            this.NomEquipe = nomEquipe;
+            this.Placement = placement;
+            this.Kill = kill;
+        }
+
+        public string NomEquipe { get => nomEquipe; set => nomEquipe = value; }
+        public int Placement { get => placement; set => placement = value; }
+        public int Kill { get => kill; set => kill = value; }
+        public override string ToString()
+        {
+            return $"{NomEquipe} {Placement} {Kill}";
+        }
     }
+
 }
