eb6b9ece358efc1d83a65a29d6e30a6492336902
Blazer54 <pmf.marques.gva@gmail.com>
2024-01-07 18:53:14 +0100

    fix bug form ajouter partie

--- ProjetPerso/FrmAjouterunepartie.Designer.cs
+++ ProjetPerso/FrmAjouterunepartie.Designer.cs
@@ -29,22 +29,51 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormAjouterPartie));
+            this.lblAjouterPartie = new System.Windows.Forms.Label();
+            this.btnValider = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // lblAjouterPartie
+            // 
+            this.lblAjouterPartie.AutoSize = true;
+            this.lblAjouterPartie.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAjouterPartie.Location = new System.Drawing.Point(337, 30);
+            this.lblAjouterPartie.Name = "lblAjouterPartie";
+            this.lblAjouterPartie.Size = new System.Drawing.Size(289, 39);
+            this.lblAjouterPartie.TabIndex = 0;
+            this.lblAjouterPartie.Text = "Ajouter une partie";
+            // 
+            // btnValider
+            // 
+            this.btnValider.Location = new System.Drawing.Point(785, 30);
+            this.btnValider.Name = "btnValider";
+            this.btnValider.Size = new System.Drawing.Size(174, 39);
+            this.btnValider.TabIndex = 1;
+            this.btnValider.Text = "Valider";
+            this.btnValider.UseVisualStyleBackColor = true;
+            this.btnValider.Click += new System.EventHandler(this.btnValider_Click);
             // 
             // FormAjouterPartie
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(998, 624);
+            this.Controls.Add(this.btnValider);
+            this.Controls.Add(this.lblAjouterPartie);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "FormAjouterPartie";
-            this.Text = "Form4";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Ajouter une partie";
             this.Load += new System.EventHandler(this.FormAjouterPartie_Load_1);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblAjouterPartie;
+        private System.Windows.Forms.Button btnValider;
     }
 }
--- ProjetPerso/FrmAjouterunepartie.cs
+++ ProjetPerso/FrmAjouterunepartie.cs
@@ -30,7 +30,63 @@ namespace ProjetPerso
 
         private void FormAjouterPartie_Load_1(object sender, EventArgs e)
         {
+            int posY = 80;
 
+            for (int i = 0; i <= equipes.Count / 2 - 1; i++)
+            {
+                //on définit une variable qui va être la position de x des éléments
+                int posX = 180;
+                // Créer un Label
+                Label label = new Label();
+                //on dit que le texte du label sera le nom du joueur à l'index qui est i
+                label.Text = $"{equipes[i].NomEquipe}";
+                //on donne un nom pour le label
+                label.Name = "lblJoueur" + i.ToString();
+                //on donne la position de où est ce que le label apparaitra
+                label.Location = new System.Drawing.Point(posX + 25, posY);
+                this.Controls.Add(label);
+
+                //on ajoute une nouvelle textbox
+                TextBox textBoxJoueur = new TextBox();
+                //on définit la position
+                textBoxJoueur.Location = new System.Drawing.Point(posX + 125, posY);
+                textBoxJoueur.Size = new System.Drawing.Size(100, 20);
+                textBoxJoueur.Name = "tbxJoueur" + i.ToString(); // Nommer la TextBox de manière unique
+                this.Controls.Add(textBoxJoueur);
+            }
+
+
+
+            for (int i = equipes.Count / 2; i <= equipes.Count - 1; i++)
+            {
+                //déclaration de la variable pour la cordonée X
+                int posX = 250;
+                // Créer un Label
+                Label label = new Label();
+                //on définit ses propriétées
+                label.Text = $"{equipes[i].NomEquipe}";
+                label.Name = "lblJoueur" + i.ToString();
+                label.Location = new System.Drawing.Point(posX + 275, posY);
+                //on l'ajoute à la liste des labels
+                this.Controls.Add(label);
+
+                //on créer une nouvelle textebox
+                TextBox textBoxJoueur = new TextBox();
+                //on indique la position de l'élément
+                textBoxJoueur.Location = new System.Drawing.Point(posX + 350, posY);
+                textBoxJoueur.Size = new System.Drawing.Size(100, 20);
+                textBoxJoueur.Name = "tbxJoueur" + i.ToString(); // Nommer la TextBox de manière unique
+                this.Controls.Add(textBoxJoueur);
+
+                posY = posY + 50;
+            }
+        }
+
+        private void btnValider_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            FrmMenuPrincipale frmMenuPrincipale = new FrmMenuPrincipale(systpoint, equipes, joueurs);
+            frmMenuPrincipale.ShowDialog();
         }
     }
 }
